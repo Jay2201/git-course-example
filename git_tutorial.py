11c28bddee10cce385b94751937ab77a63176acb
JM <jaymangi99@gmail.com>
2024-08-27 11:54:23 +0530

    formatted code

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,6 +1,7 @@
 def add_number(a, b, c):
-    """ Returns summation of a, b and c"""
+    """Returns summation of a, b and c"""
     return sum([a + b + c])
+
 
 def calc_difference(a, b):
     return a - b
